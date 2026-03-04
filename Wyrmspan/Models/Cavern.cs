cb68fe5dd3ac304c575755bb1be08cb5e49ab05b
Simón Sánchez-Soto <ssanchez-soto2@huskers.unl.edu>
2026-03-04 15:44:13 -0600

    Completed Cavern

--- Wyrmspan/Models/Cavern.cs
+++ Wyrmspan/Models/Cavern.cs
@@ -1,27 +1,134 @@
 class Cavern {
     // Constants
     const int CAVES_PER_CAVERN = 4;
+    const int MAX_EXPLORE_COUNT = 3;
 
     // Non-Constants
     int exploreCount;
     Cave[] caves;
+    int caveIndex;
     Dragon[] dragons;
-    Action capstoneReward;
+    int dragonIndex;
+    WyrmAction capstoneReward;
     int totEggCapacity;
 
     /*
-    Adds a Cave to this Cavern. Returns the Action of the Cave, and the capstone Action of this Cavern, if applicable.
+    Adds a Cave to this Cavern. Returns the WyrmAction of the Cave, and the capstone WyrmAction of this Cavern, if applicable.
     Assumes validation is done by the caller.
 
     Parameters:
     c: the Cave to be added
 
     Return:
-    an Action[] containing the action of the placed Cave, and if the fourth Cave is placed, the Action of this Cavern as well.
+    a WyrmAction[] containing the WyrmAction of the placed Cave, and if the fourth Cave is placed, the WyrmAction of this Cavern as well.
     */
-    // TODO: finish this
-    // public Action[] addCave(Cave c)
-    // {
-        
-    // }
+    public WyrmAction[] addCave(Cave c) {
+        this.caves[this.caveIndex] = c;
+        this.caveIndex++;
+
+        if (this.caveIndex >= 4) {
+            return [c.getAction(), this.capstoneReward];
+        }
+
+        return [c.getAction()];
+    }
+
+    /*
+    Adds a Dragon to this Cavern. Returns the WyrmAction of the Dragon, if applicable.
+    Assumes validation is done by the caller.
+
+    Parameters:
+    d: the Dragon to be added
+
+    Return:
+    a WyrmAction[] containing the WyrmAction of the placed Dragon, if applicable.
+    */
+    public WyrmAction[] addDragon(Dragon d) {
+        this.dragons[this.dragonIndex] = d;
+        this.dragonIndex++;
+
+        return [d.getAction()];
+    }
+
+    /*
+    Returns the list of WyrmActions 
+
+    Return:
+    a WyrmActionp[] containing the WyrmActions of the placed Dragons
+    */
+    public WyrmAction[] explore() {
+        if (this.dragonIndex == 0 || this.exploreCount >= MAX_EXPLORE_COUNT) {
+            return [];
+        }
+
+        this.exploreCount++;
+
+        WyrmAction[] output = new WyrmAction[this.dragonIndex];
+
+        for (int i = 0; i < this.dragonIndex; i++) {
+            output[i] = this.dragons[i].getAction();
+        }
+
+        return output;
+    }
+
+    /*
+    Getter for total egg capacity
+    */
+    public int getTotEggCapacity() {
+        this.calculateCapacity();
+        return this.totEggCapacity;
+    }
+
+    /*
+    Calculates the total egg capacity of all dragons within
+    */
+    private void calculateCapacity() {
+        this.totEggCapacity = 0;
+        for (int i = 0; i < this.dragonIndex; i++) {
+            this.totEggCapacity += this.dragons[i].getEggCapacity();
+        }
+    }
+
+    /*
+    Getter for Caves
+    */
+    public Cave[] getCaves() {
+        return this.caves;
+    }
+
+    /*
+    Getter for Dragons
+    */
+    public Dragon[] GetDragons() {
+        return this.dragons;
+    }
+
+    /*
+    Getter for cave count
+    */
+    public int getCaveCount() {
+        return this.caveIndex;
+    }
+
+    /*
+    Getter for dragon count
+    */
+    public int getDragonCount() {
+        return this.dragonIndex;
+    }
+
+    /*
+    Getter for explore count
+    */
+    public int getExploreCount() {
+        return this.exploreCount;
+    }
+
+    /*
+    Getter for capstone action
+    */
+    public WyrmAction getCapstoneAction() {
+        return this.capstoneReward;
+    }
 }
